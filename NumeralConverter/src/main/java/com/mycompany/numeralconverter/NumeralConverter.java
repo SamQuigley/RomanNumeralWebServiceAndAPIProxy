f157ae2c6219c73a4e52ea77fed8e5be5ed9e6cf
samquigley <samq2011@live.com>
2017-11-07 14:19:20 +0000

    making directory for part 2

--- NumeralConverter/src/main/java/com/mycompany/numeralconverter/NumeralConverter.java
+++ NumeralConverter/src/main/java/com/mycompany/numeralconverter/NumeralConverter.java
@@ -5,10 +5,25 @@
  */
 package com.mycompany.numeralconverter;
 
+
+import java.util.Hashtable;
+
+
 /**
  *
  * @author samq2
  */
-public class NumeralConverter {
+public class NumeralConverter {   Hashtable<Integer, String> ht = new Hashtable<Integer, String>();
+        ht.put(1, "I");
+        hashtable.put(4, "IV");
+        hashtable.put(9, "IX");
+        hashtable.put(10, "X");
+        hashtable.put(40, "XL");
+        hashtable.put(90, "XC");
+        hashtable.put(400, "CD");
+        hashtable.put(500, "D");
+        hashtable.put(1000, "M");
+        
+    
     
 }
